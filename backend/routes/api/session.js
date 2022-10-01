d99600bfb53240e5c29a686e2c03d51c1dc84dbb
jake matillano <jakematillano.z@gmail.com>
2022-10-01 15:00:06 -0700

    user endpoints working

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -16,10 +16,8 @@ const validateLogin = [
     .withMessage('Please provide a password.'),
   handleValidationErrors
 ];
-router.post(
-  '/',
-  validateLogin,
-  async (req, res, next) => {
+
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
@@ -39,10 +37,7 @@ router.post(
     });
   }
 );
-router.get(
-  '/',
-  restoreUser,
-  (req, res) => {
+router.get('/',restoreUser,(req, res) => {
     const { user } = req;
     if (user) {
       return res.json({
@@ -52,32 +47,8 @@ router.get(
   }
 );
 
-router.post(
-  '/',
-  async (req, res, next) => {
-    const { credential, password } = req.body;
 
-    const user = await User.login({ credential, password });
-
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
-    }
-
-    await setTokenCookie(res, user);
-
-    return res.json({
-      user
-    });
-  }
-);
-
-router.delete(
-  '/',
-  (_req, res) => {
+router.delete('/',(_req, res) => {
     res.clearCookie('token');
     return res.json({ message: 'success' });
   }
