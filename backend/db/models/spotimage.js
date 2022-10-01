397e2833a8c0920b9633337de37cf2e0b2c00865
jake matillano <jakematillano.z@gmail.com>
2022-10-01 13:21:48 -0700

    relationships done

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -11,11 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      SpotImage.belongsTo(models.Spot,{foreignKey:"spotId"})
     }
   }
   SpotImage.init({
     id: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-    spotId: {type: DataTypes.INTEGER, onDelete:"CASCADE"},
+    spotId: {type: DataTypes.INTEGER, onDelete:"CASCADE", references:{model:"Spots", key:"id"}},
     url: {type: DataTypes.STRING,},
     preview: {type: DataTypes.BOOLEAN},
   }, {
