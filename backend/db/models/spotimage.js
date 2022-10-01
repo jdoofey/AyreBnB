c72bfdb6529e730daa3c8c3de8c6da1556a2228d
jake matillano <jakematillano.z@gmail.com>
2022-10-01 13:23:30 -0700

    tweaks for syntax

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -22,6 +22,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'SpotImage',
+    defaultScope:{attributes:{exclude:["createdAt","updatedAt"]}}
   });
   return SpotImage;
 };
