fb6dccea6763f0fa46f8d838f6e8d9647f9f1cfe
jake matillano <jakematillano.z@gmail.com>
2022-10-03 22:08:54 -0700

    working on login

---
+++ frontend/src/store/session.js
@@ -0,0 +1,17 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
