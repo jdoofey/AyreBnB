be5b5ee047d6019605259592b3d51e71631a5046
jake matillano <jakematillano.z@gmail.com>
2022-10-04 16:51:06 -0700

    login modal checkpoint

--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom';
 import './Modal.css';
 
 const ModalContext = React.createContext();
+
+export function ModalProvider({ children }) {
+  const modalRef = useRef();
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    setValue(modalRef.current);
+  }, [])
