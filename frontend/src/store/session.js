759129796cabf71257cb3f315d5fa592584edf9a
jake matillano <jakematillano.z@gmail.com>
2022-10-03 23:10:24 -0700

    added first and last names as requirement to signup frontend

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,19 +17,22 @@ const removeUser = () => {
 };
 
 export const signup = (user) => async (dispatch) => {
-  const { username, email, password } = user;
+  const { username, email, password, firstName, lastName } = user;
   const response = await csrfFetch("/api/users", {
     method: "POST",
     body: JSON.stringify({
       username,
       email,
       password,
+      firstName,
+      lastName
     }),
   });
   const data = await response.json();
   dispatch(setUser(data.user));
   return response;
 };
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
