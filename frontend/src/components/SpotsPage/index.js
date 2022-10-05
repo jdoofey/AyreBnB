65aa53856c14dcdb4cec01aa5938b26c8b6db524
jake matillano <jakematillano.z@gmail.com>
2022-10-04 23:35:43 -0700

    starting spotspage component, moving to new branch

---
+++ frontend/src/components/SpotsPage/index.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllSpots } from "../../store/spots";
+import './SpotsPage.css'
+
+const SpotsPage = () => {
+  const dispatch = useDispatch()
+}
