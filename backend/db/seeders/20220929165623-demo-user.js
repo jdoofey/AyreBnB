36a3e02c14a4f81955f737dfd0050b758205423b
jake matillano <jakematillano.z@gmail.com>
2022-10-05 23:19:03 -0700

    adding seeders

--- backend/db/seeders/20220929165623-demo-user.js
+++ backend/db/seeders/20220929165623-demo-user.js
@@ -24,7 +24,56 @@ module.exports = {
         firstName: "David",
         lastName:"Rogers",
         hashedPassword: bcrypt.hashSync('password')
-      }
+      },
+      {
+        email: 'user3@user.io',
+        username: 'suhspect',
+        firstName: "Samuel",
+        lastName:"Sammyson",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user4@user.io',
+        username: 'garebare',
+        firstName: "Gary",
+        lastName:"Garard",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user5@user.io',
+        username: 'goodreg',
+        firstName: "John",
+        lastName:"Johnestan",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user6@user.io',
+        username: 'log(n)',
+        firstName: "Logan",
+        lastName:"Richardson",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user7@user.io',
+        username: 'duhmanda',
+        firstName: "Amanda",
+        lastName:"Amananda",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user8@user.io',
+        username: 'GwaiPut',
+        firstName: "Alex",
+        lastName:"Alexandria",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user9@user.io',
+        username: '5two1FTW',
+        firstName: "Brandon",
+        lastName:"Brandy",
+        hashedPassword: bcrypt.hashSync('password')
+      },
     ], {});
   },
 
