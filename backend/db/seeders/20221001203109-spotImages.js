13bef04a422966df3ca6a0467c6fed6482b12bb3
jake matillano <jakematillano.z@gmail.com>
2022-10-05 23:04:59 -0700

    adding seeders

--- backend/db/seeders/20221001203109-spotImages.js
+++ backend/db/seeders/20221001203109-spotImages.js
@@ -40,26 +40,51 @@ module.exports = {
     },
     {
       spotId:2,
-      url:"",
+      url:"https://images.unsplash.com/photo-1517541866997-ea18e32ea9e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+      preview:true
+    },
+    {
+      spotId:2,
+      url:"https://images.unsplash.com/photo-1625647999883-376f8f48c52a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80",
       preview:false
     },
     {
       spotId:2,
-      url:"",
+      url:"https://images.unsplash.com/photo-1588854337236-6889d631faa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       preview:false
     },
     {
       spotId:2,
-      url:"",
+      url:"https://images.unsplash.com/photo-1598928636135-d146006ff4be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       preview:false
     },
     {
       spotId:2,
+      url:"https://images.unsplash.com/photo-1584622650111-993a426fbf0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      preview:false
+    },
+    {
+      spotId:3,
+      url:"https://mediacloud.theweek.com/image/upload/f_auto,t_content-image-desktop@1/v1645734537/TWS1068.props.sherman.ext.jpg",
+      preview:true
+    },
+    {
+      spotId:3,
+      url:"https://plus.unsplash.com/premium_photo-1661950439212-558fa5cc82e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1151&q=80",
+      preview:false
+    },
+    {
+      spotId:3,
       url:"",
       preview:false
     },
     {
-      spotId:2,
+      spotId:3,
+      url:"",
+      preview:false
+    },
+    {
+      spotId:3,
       url:"",
       preview:false
     },
