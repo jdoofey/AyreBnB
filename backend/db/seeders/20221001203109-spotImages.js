21334b619ae3bef1164e641e8f213449b14dce06
jake matillano <jakematillano.z@gmail.com>
2022-10-05 23:59:32 -0700

    adding seeders

--- backend/db/seeders/20221001203109-spotImages.js
+++ backend/db/seeders/20221001203109-spotImages.js
@@ -138,6 +138,11 @@ module.exports = {
       url:"https://images.unsplash.com/photo-1507089947368-19c1da9775ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
       preview:false
     },
+    {
+      spotId:5,
+      url:"",
+      preview:true
+    },
 
    ])
   },
